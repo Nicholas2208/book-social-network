41e574776c2040276903499c260b9315dbd8e091
Nicholas2208 <nikolaj.belyakov.84@mail.ru>
2024-08-25 18:39:32 +0300

    initial commit

---
+++ src/main/java/com/nwhite/book/history/BookTransactionHistory.java
@@ -0,0 +1,30 @@
+package com.nwhite.book.history;
+
+import com.nwhite.book.book.Book;
+import com.nwhite.book.common.BaseEntity;
+import com.nwhite.book.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+    private boolean returned;
+    private boolean returnApproved;
+}
